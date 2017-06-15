72d6ef2b3db5c678e612b918802ca3b6741bffe6
chung.triniti@gmail.com <johndoe@example.com>
2017-06-15 17:41:31 +0800

    Add reinstate state

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -3,12 +3,16 @@ Spree::Payment.class_eval do
   INVALID_STATES      = %w(failed invalid disputed).freeze
 
   scope :disputed, -> { with_state('disputed') }
+  scope :reinstated, -> { with_state('reinstated') }
   
   # Redefine state 
   state_machine initial: :checkout do
-    state :disputed
+    state :disputed, :reinstated
     event :dispute do
       transition from: [:completed], to: :disputed
+    end
+    event :reinstate do 
+      transition from: [:disputed], to: :reinstated
     end
 
     after_transition to: :disputed do |payment, transition|
